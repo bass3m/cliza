b23462b15db4ed87f0fbec09498c0ff6994880a1
Bassem Youssef <bassem@kumunetworks.com>
2015-03-21 12:33:10 -0700

    cliza: initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject cliza "0.1.0-SNAPSHOT"
+  :description "Eliza like in Clojure"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])

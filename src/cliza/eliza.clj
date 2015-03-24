1a363509821c1d07a3e12bbb072b9fbf2cacf54b
Bassem Youssef <bassem@kumunetworks.com>
2015-03-24 08:47:44 -0700

    cliza : more matches

--- src/cliza/eliza.clj
+++ src/cliza/eliza.clj
@@ -1,37 +1,59 @@
 (ns cliza.eliza)
 
 (def eliza-rules
-  {"?*x hello ?*y" ["How do you do. Please state your problem."]
-   "?*x I want ?*y" ["What would it mean if you got %y"
-                     "Why do you want %y ?"
-                     "Suppose you got %y soon"]
-   "?*x if ?*y" ["Do you really think its likely that %y"
-                 "Do you wish that %y"
-                 "What do you think about %y"
-                 "Really -- if %y"]
-   "?*x no ?*y" ["Why not?"
-                 "You are being a bit negative"
-                 "Are you saying NO just to be negative?"]
-   "?*x I was ?*y" ["Were you really?"
-                    "Perhaps I already knew you were %y"
-                    "Why do you tell me you were %y now?"]
-   "?*x I feel ?*y" ["Do you often feel %y ?"]
-   "?*x I felt ?*y" ["What other feelings do you have ?"]
-   "?*x sorry ?*y" ["Please don't apologise."
-                    "Apologies are not necessary."]
-   "?*x i remember ?*y" ["Do you often think of %y ?"
-                         "Does thinking of %y bring anything else to mind ?"
-                         "What else do you recollect ?"
-                         "Why do you recollect %y just now ?"
-                         "What in the present situation reminds you of %y ?"
-                         "What is the connection between me and %y ?"]
-   "?*x do you remember ?*y" ["Did you think I would forget %y ?"
-                              "Why do you think I should recall %y now ?"
-                              "What about %y ?"
-                              "You mentioned %y ?"]
-   "?*x i dreamed ?*y" ["Really, %y ?"
-                        "Have you ever fantasized %y while you were awake ?"
-                        "Have you ever dreamed %y before ?"]
+  {"?*x hello ?*y"
+   ["How do you do. Please state your problem."]
+   "?*x I want ?*y"
+   ["What would it mean if you got %y"
+    "Why do you want %y ?"
+    "Suppose you got %y soon"]
+   "?*x if ?*y"
+   ["Do you really think its likely that %y"
+    "Do you wish that %y"
+    "What do you think about %y"
+    "Really -- if %y"]
+   "?*x no ?*y"
+   ["Why not?"
+    "You are being a bit negative"
+    "Are you saying NO just to be negative?"]
+   "?*x I was ?*y"
+   ["Were you really?"
+    "Perhaps I already knew you were %y"
+    "Why do you tell me you were %y now?"]
+   "?*x I feel ?*y"
+   ["Do you often feel %y ?"]
+   "?*x I felt ?*y"
+   ["What other feelings do you have ?"]
+   "?*x sorry ?*y"
+   ["Please don't apologise."
+    "Apologies are not necessary."]
+   "?*x i remember ?*y"
+   ["Do you often think of %y ?"
+    "Does thinking of %y bring anything else to mind ?"
+    "What else do you recollect ?"
+    "Why do you recollect %y just now ?"
+    "What in the present situation reminds you of %y ?"
+    "What is the connection between me and %y ?"]
+   "?*x do you remember ?*y"
+   ["Did you think I would forget %y ?"
+    "Why do you think I should recall %y now ?"
+    "What about %y ?"
+    "You mentioned %y ?"]
+   "?*x i dreamed ?*y"
+   ["Really, %y ?"
+    "Have you ever fantasized %y while you were awake ?"
+    "Have you ever dreamed %y before ?"]
+   "?*x dream ?*y"
+   ["What does that dream suggest to you ?"
+    "Do you dream often ?"
+    "What persons appear in your dreams ?"
+    "Do you believe that dreams have something to do with your problems ?"]
+   "?*x perhaps ?*y"
+   ["You don't seem quite certain."
+    "Why the uncertain tone ?"
+    "Can't you be more positive ?"
+    "You aren't sure ?"
+    "Don't you know ?"]
    })
 
 (def dflt-question-resps ["I have no idea."

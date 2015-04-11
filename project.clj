87eb85e3f398a72d6b7c88c616b33fa939a540a4
Bassem Youssef <bassem@kumunetworks.com>
2015-04-11 11:36:48 -0700

    cliza : add a main to the project so it's standalone

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Eliza from PAIP in Clojure"
   :url "https://github.com/bass3m/cliza"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main cliza.eliza)

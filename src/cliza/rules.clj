f3ce6a62788f3840c16a20d3f16b1418ff6a5ad1
Bassem Youssef <bassem@kumunetworks.com>
2015-04-27 09:12:17 -0700

    cliza : sports synonyms

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -460,7 +460,8 @@
    "swear" #{"fuck" "fucker" "shit" "damn"}
    "sports" #{"atheletics" "baseball" "basketball" "football" "frisbee"
               "gym" "gymnastics" "hockey" "lacrosse" "soccer" "softball"
-              "sports" "swimming" "swim" "tennis" "volleyball" "soccer"}
+              "sports" "swimming" "swim" "tennis" "volleyball" "soccer"
+              "nfl" "nba"}
    "sex" #{"fucked" "screw" "screwing" "fucking" "rape" "raped"
            "kiss" "kissing" "kisses" "screws" "fucks"}
    "problems" #{"inhibitions" "hangups" "difficulties"

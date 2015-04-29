834cce9d64ffc02db75b997733cd2213e4bb43f6
Bassem Youssef <bassem@kumunetworks.com>
2015-04-29 07:09:07 -0700

    cliza : sports

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -461,7 +461,7 @@
    "sports" #{"atheletics" "baseball" "basketball" "football" "frisbee"
               "gym" "gymnastics" "hockey" "lacrosse" "soccer" "softball"
               "sports" "swimming" "swim" "tennis" "volleyball" "nfl" "nba"
-              "cricket"}
+              "cricket" "rugby"}
    "sex" #{"fucked" "screw" "screwing" "fucking" "rape" "raped"
            "kiss" "kissing" "kisses" "screws" "fucks"}
    "problems" #{"inhibitions" "hangups" "difficulties"

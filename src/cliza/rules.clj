b69a6752051340fffd73001eeccef95cd480f237
Bassem Youssef <bassem@kumunetworks.com>
2015-05-05 06:40:51 -0700

    cliza : additional words in set

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -461,7 +461,7 @@
    "sports" #{"atheletics" "baseball" "basketball" "football" "frisbee"
               "gym" "gymnastics" "hockey" "lacrosse" "soccer" "softball"
               "sports" "swimming" "swim" "tennis" "volleyball" "nfl" "nba"
-              "cricket" "rugby" "boxing" "wrestling" "mma"}
+              "cricket" "rugby" "boxing" "wrestling" "mma" "foosball"}
    "sex" #{"fucked" "screw" "screwing" "fucking" "rape" "raped"
            "kiss" "kissing" "kisses" "screws" "fucks"}
    "problems" #{"inhibitions" "hangups" "difficulties"

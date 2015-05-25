a6259df6bcbccf358c9f84684ea58dda51b9ca83
Bassem Youssef <bassem@kumunetworks.com>
2015-05-24 20:36:30 -0700

    cliza : linux distro

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -453,8 +453,8 @@
    "want" #{"need" "desire"}
    "computer" #{"mac" "linux" "pc" "osx" "ubuntu" "c#" "scala" "coffeescript"
                 "fpga" "virtex" "asic" "lenovo" "c++" "lisp" "java" "centos" "fedora"
-                "python" "perl" "clojure" "javascript" "prolog" "haskell" "archlinux"
-                "erlang" "cobol" "julia" "fortran" "pascal" "purescript"}
+                "centos" "python" "perl" "clojure" "javascript" "prolog" "haskell"
+                "archlinux" "erlang" "cobol" "julia" "fortran" "pascal" "purescript"}
    "everyone" #{"noone" "everybody" "nobody"}
    "sad" #{"unhappy" "depressed" "sick" "miserable"}
    "swear" #{"fuck" "fucker" "shit" "damn"}

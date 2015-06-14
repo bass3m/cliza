dde7d0ae1fa6daec269503307b7fafaa36c266a6
Bassem Youssef <bassem@kumunetworks.com>
2015-06-14 09:49:26 -0700

    cliza : luajit

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -457,7 +457,7 @@
                 "archlinux" "erlang" "cobol" "julia" "fortran" "pascal" "purescript"
                 "redhat" "lisp" "smalltalk" "nim" "numpy" "scipy" "reactjs" "angularjs"
                 "meteor" "knockoutjs" "meteorjs" "postgres" "couchdb" "mongodb" "riak"
-                "mariadb" "sqlite" "idris" "lua"}
+                "mariadb" "sqlite" "idris" "lua" "luajit"}
    "everyone" #{"noone" "everybody" "nobody"}
    "sad" #{"unhappy" "depressed" "sick" "miserable"}
    "swear" #{"fuck" "fucker" "shit" "damn"}

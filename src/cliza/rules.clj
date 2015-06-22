497e22c025ec85253c087a8e447e6d8dc6607cea
Bassem Youssef <bassem@kumunetworks.com>
2015-06-22 08:17:54 -0700

    cliza/db : add redis

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -458,7 +458,7 @@
                 "redhat" "lisp" "smalltalk" "nim" "numpy" "scipy" "reactjs" "angularjs"
                 "meteor" "knockoutjs" "meteorjs" "postgres" "couchdb" "mongodb" "riak"
                 "mariadb" "sqlite" "idris" "lua" "luajit" "rethinkdb" "datomic" "mysql"
-                "ravendb" "typescript" "dart"}
+                "ravendb" "typescript" "dart" "redis"}
    "everyone" #{"noone" "everybody" "nobody"}
    "sad" #{"unhappy" "depressed" "sick" "miserable"}
    "swear" #{"fuck" "fucker" "shit" "damn"}

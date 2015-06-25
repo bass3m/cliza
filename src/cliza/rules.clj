39af7f9d939c889781592755b4ac3c6af5eaca08
Bassem Youssef <bassem@kumunetworks.com>
2015-06-25 06:17:03 -0700

    cliza/db : more db

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -458,7 +458,7 @@
                 "redhat" "lisp" "smalltalk" "nim" "numpy" "scipy" "reactjs" "angularjs"
                 "meteor" "knockoutjs" "meteorjs" "postgres" "couchdb" "mongodb" "riak"
                 "mariadb" "sqlite" "idris" "lua" "luajit" "rethinkdb" "datomic" "mysql"
-                "ravendb" "typescript" "dart" "redis" "agda" "coq"}
+                "ravendb" "typescript" "dart" "redis" "agda" "coq" "memcachd"}
    "everyone" #{"noone" "everybody" "nobody"}
    "sad" #{"unhappy" "depressed" "sick" "miserable"}
    "swear" #{"fuck" "fucker" "shit" "damn"}

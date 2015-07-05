825af4f473e9dd46cce052b6210c39127541289c
Bassem Youssef <bassem@kumunetworks.com>
2015-07-05 07:27:02 -0700

    cliza : azure

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -459,7 +459,7 @@
                 "meteor" "knockoutjs" "meteorjs" "postgres" "couchdb" "mongodb" "riak"
                 "mariadb" "sqlite" "idris" "lua" "luajit" "rethinkdb" "datomic" "mysql"
                 "ravendb" "typescript" "dart" "redis" "agda" "coq" "memcachd" "pouchdb"
-                "backbonejs" "golang" "scheme" "ocaml" "sml" "f#" "rust" "aws"}
+                "backbonejs" "golang" "scheme" "ocaml" "sml" "f#" "rust" "aws" "azure"}
    "everyone" #{"noone" "everybody" "nobody"}
    "sad" #{"unhappy" "depressed" "sick" "miserable"}
    "swear" #{"fuck" "fucker" "shit" "damn"}

d64bec6e4ab71dd6b81dea2f34d5d5fce2358aca
Bassem Youssef <bassem@kumunetworks.com>
2015-07-30 08:53:57 -0700

    cliza : mathematica

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -462,7 +462,7 @@
                 "backbonejs" "golang" "scheme" "ocaml" "sml" "f#" "rust" "aws" "azure" "heroku"
                 "dynamodb" "iojs" "forth" "paxos" "hadoop" "etcd" "zookeeper" "jquery" "drupal"
                 "solr" "lucene" "coreos" "cassandradb" "slackware" "json" "yaml" "xml" "bigml"
-                "wordpress" "matlab"}
+                "wordpress" "matlab" "mathematica"}
    "everyone" #{"noone" "everybody" "nobody"}
    "sad" #{"unhappy" "depressed" "sick" "miserable"}
    "swear" #{"fuck" "fucker" "shit" "damn"}

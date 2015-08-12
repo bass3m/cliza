81a122456910014f5846239d018674b5611382e8
Bassem Youssef <bassem@kumunetworks.com>
2015-08-12 06:22:19 -0700

    cliza : axure

--- src/cliza/rules.clj
+++ src/cliza/rules.clj
@@ -463,7 +463,7 @@
                 "dynamodb" "iojs" "forth" "paxos" "hadoop" "etcd" "zookeeper" "jquery" "drupal"
                 "solr" "lucene" "coreos" "cassandradb" "slackware" "json" "yaml" "xml" "bigml"
                 "wordpress" "matlab" "mathematica" "octave" "mahout" "neo4j" "mesos" "dynamodb"
-                "oozie" "elasticsearch" "karaf" "mesosphere" "unity" "ansible"}
+                "oozie" "elasticsearch" "karaf" "mesosphere" "unity" "ansible" "axure"}
    "everyone" #{"noone" "everybody" "nobody"}
    "sad" #{"unhappy" "depressed" "sick" "miserable"}
    "swear" #{"fuck" "fucker" "shit" "damn" "bastard"}
